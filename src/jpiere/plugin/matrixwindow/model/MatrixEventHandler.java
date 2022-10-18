f65a430e4891083a485a8711403ec452af0d41be
AndreaCh83 <andrew1buffon@gmail.com>
2022-10-18 12:03:42 +0200

    aChecchia: plug-in jpiereMatrixWindow --- Visualizzazione in griglia a rottura di categoria mercelogica + Totali progressivi. Modifiche fatte SOLO per CLIENTE Limestone.  _UPD4

--- src/jpiere/plugin/matrixwindow/model/MatrixEventHandler.java
+++ src/jpiere/plugin/matrixwindow/model/MatrixEventHandler.java
@@ -10,6 +10,7 @@ import org.compiere.model.PO;
 import org.compiere.model.Query;
 import org.compiere.util.DB;
 import org.compiere.util.Env;
+import org.compiere.util.Trx;
 import org.osgi.service.event.Event;
 
 public class MatrixEventHandler extends AbstractEventHandler {
@@ -19,33 +20,39 @@ public class MatrixEventHandler extends AbstractEventHandler {
 		if(DB.isTableOrViewExists("LIT_M_Product_Category")) {
 			registerTableEvent(IEventTopics.PO_AFTER_NEW, "M_InOutLine");
 			registerTableEvent(IEventTopics.PO_AFTER_CHANGE, "M_InOutLine");
+			registerTableEvent(IEventTopics.PO_BEFORE_CHANGE, "M_InOutLine");
+			registerTableEvent(IEventTopics.PO_BEFORE_NEW, "M_InOutLine");
 		}
 	}
 
 	@Override
 	protected void doHandleEvent(Event event) {
-		if(event.getTopic().equals(IEventTopics.PO_AFTER_NEW) || event.getTopic().equals(IEventTopics.PO_AFTER_CHANGE)){
-			PO po = getPO(event);
-			
-			if(po.get_TableName().equals("M_InOutLine") && po.columnExists("LIT_M_Product_Category_ID") && po.columnExists("LineNetAmt")) {
-				try {
-					DB.commit(false, po.get_TrxName());
-				} catch (IllegalStateException e) {
-					e.printStackTrace();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
+		PO po = getPO(event);
+		if(po.get_TableName().equals("M_InOutLine") && po.columnExists("LIT_M_Product_Category_ID") && po.columnExists("LineNetAmt") && po.is_ValueChanged("QtyEntered")) {
+			if(event.getTopic().equals(IEventTopics.PO_AFTER_NEW) || event.getTopic().equals(IEventTopics.PO_AFTER_CHANGE)){
+
+				int adClientID = po.getAD_Client_ID();
 				Timestamp shipDate = (Timestamp)po.get_Value("ShipDate");
-				BigDecimal sumLineNetAmt = new Query(Env.getCtx(), "M_InOutLine", "ShipDate=? AND LIT_M_Product_Category_ID=?", null)
+				//Per problemi di RELEASE SAVEPOINT, calcolo manuale dell'ultimo dato inserito
+				BigDecimal sumLineNetAmt = new Query(Env.getCtx(), "M_InOutLine", "ShipDate=? AND LIT_M_Product_Category_ID=? AND M_InOutLine_ID !=?", null)
 						.setClient_ID()
 						.setOnlyActiveRecords(true)
-						.setParameters(shipDate, po.get_ValueAsInt("LIT_M_Product_Category_ID"))
+						.setParameters(shipDate, po.get_ValueAsInt("LIT_M_Product_Category_ID"), po.get_ValueAsInt("M_InOutLine_ID"))
 						.sum("LineNetAmt");
-				int adClientID = po.getAD_Client_ID();
+				BigDecimal addAmt = BigDecimal.ZERO;
+				if(((BigDecimal)po.get_Value("QtyEntered")).compareTo(BigDecimal.ZERO)>0)
+					addAmt = (BigDecimal)po.get_Value("LineNetAmt");
+				sumLineNetAmt = sumLineNetAmt.add(addAmt);
+				////////
+				
 				String sqlUpdate = "UPDATE M_InOutLine SET QtyEntered=? WHERE AD_Client_ID=? AND ShipDate=? "
 						+ "AND LIT_M_Product_Category_ID IN (SELECT M_Product_Category_Parent_ID FROM LIT_M_Product_Category WHERE AD_Client_ID=? AND LIT_M_Product_Category_ID=?)";
 				DB.executeUpdate(sqlUpdate, new Object[] {sumLineNetAmt, adClientID, shipDate, adClientID, po.get_ValueAsInt("LIT_M_Product_Category_ID")}, false, null);
 				
+			}
+			else if(event.getTopic().equals(IEventTopics.PO_BEFORE_NEW) || event.getTopic().equals(IEventTopics.PO_BEFORE_CHANGE)){
+				if(((BigDecimal)po.get_Value("QtyEntered")).compareTo(BigDecimal.ZERO)<=0)
+					po.set_ValueOfColumn("LineNetAmt", BigDecimal.ZERO);
 			}
 		}
 	}
